be180a0a294103d2e3c8b474ed5a98f08a155fa9
tungue <153699315+47solar@users.noreply.github.com>
2023-12-17 17:56:22 +0300

    Update ip_lit.sh

--- ip_lit.sh
+++ ip_lit.sh
@@ -20,22 +20,23 @@ then
  
 Help list
 	
-exapmle use:
+Exapmle use:
 
-./test.sh --ban INPUT --port 80 --sring \"attack\"  //ban request \"attack\" on port 80
+./test.sh --ban INPUT --port 80 --word \"attack\"  //ban request \"attack\" on port 80
 ./test.sh --ban OUTPUT --protocol tcp --port 80     //ban requests to port 80
 ./test.sh -u INPUT 1                                //unban first chain
 
 --ban        -b         add port or word to the blacklist
-	INPUT           incoming traffic
-	OUTPUT          outgoing traffic
-	FORWARD         traffic passing through the system
---unban      -u         unlock the word
-	[name or num]               like --unban INPUT 4
+        INPUT           incoming traffic
+        OUTPUT          outgoing traffic
+        FORWARD         traffic passing through the system
+--unban      -u         unlock the word or port
+        INPUT, OUTPUT, FORWARD
+        [name or num]               like --unban INPUT 4
 --port       -p         specify the required port
---protocol   -P	        data transfer protocol (tcp, udp, etc...)
+--protocol   -P         data transfer protocol (tcp, udp, etc...)
 --word       -w         argument is the desired word
-		                    like --string \"porn\"
+                                    like --word \"porn\"
 --list       -L         displays a list of all locks"
 elif [ $1 == "--list" ]||[ $1 == "-L" ] 
 then
