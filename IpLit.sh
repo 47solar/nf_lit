12024468d1791bf31d98d519d9b0e6310ef5dbb4
tungue <153699315+47solar@users.noreply.github.com>
2024-01-07 11:24:33 +0300

    Update IpLit.sh

--- IpLit.sh
+++ IpLit.sh
@@ -23,8 +23,10 @@ Help list
 Exapmle use:
 
 ./IpLit.sh --ban INPUT --protocol udp --port 80 --word \"attack\"  //ban request \"attack\" on port 80 and protocol udp
-./IpLit.sh --ban OUTPUT --protocol tcp --port 80     		   //ban requests to port 80 and protocol tcp
-./IpLit.sh -u INPUT 1                               		   //unban first chain
+./IpLit.sh --ban OUTPUT --protocol tcp --port 80     		       //ban requests to port 80 and protocol tcp
+./IpLit.sh --unban INPUT 1                               		   //unban first chain
+./IpLit.sh --filter OUTPUT --protocol tcp --port 443 --quan 3
+Arguments:
 
 --ban        -b         add port or word to the blacklist
         INPUT           incoming traffic
@@ -33,11 +35,13 @@ Exapmle use:
 --unban      -u         unlock the word or port
         INPUT, OUTPUT, FORWARD
         [name or num]               like --unban INPUT 4
---port       -p         specify the required port
---protocol   -P         data transfer protocol (tcp, udp, etc...)
---word       -w         argument is the desired word
+    --port       -p         specify the required port
+    --protocol   -P         data transfer protocol (tcp, udp, etc...)
+    --word       -w         argument is the desired word
                                     like --word \"porn\"
---list       -L         displays a list of all locks"
+--list       -L         displays a list of all locks
+--filter     -f         limiting the number of connections to a specific port
+    --quan       -a         number of connections"
 elif [ "$1" == "--list" ]||[ "$1" == "-L" ]
 then
 	sudo /sbin/iptables --list --line-numbers 
@@ -54,6 +58,11 @@ then
 elif [ "$1" == "--unban" ]||[ "$1" == "-u" ]&&[ "$2" == "INPUT" ]||[ "$2" == "OUTPUT" ]||[ "$2" == "FORWARD" ]
 then
 	sudo /sbin/iptables -D "$2" "$3"
+	echo "
+	Completed!"
+elif [ "$1" == "--filter" ]||[ "$1" == "-f" ]&&[ "$3" == "INPUT" ]||[ "$3" == "OUTPUT" ]||[ "$3" == "FORWARD" ]&&[ "$4" == "--protocol" ]||[ "$4" == "-P" ]&&[ "$6" == "--port" ]||[ "$6" == "-p" ]&&[ "$8" == "--quan" ]||[ "$8" == "-q" ]
+then
+	sudo /sbin/iptables -t filter -I "$3" -p "$5" --syn --dport "$7" -m connlimit --connlimit-above "$9" -j DROP
 	echo "
 	Completed!"
 else
